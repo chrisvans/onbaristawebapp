c4d1ad2d3cc80f31438b74b1de4640a1e9504c40
AJLeonardi <anthony.leonardi@gmail.com>
2013-03-01 15:20:21 -0500

    added an admin page for users with the isCompanyAdmin attribute set to true

---
+++ onBaristaApp/migrations/0003_auto__add_field_userprofile_isCompanyAdmin.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'UserProfile.isCompanyAdmin'
+        db.add_column('onBaristaApp_userprofile', 'isCompanyAdmin',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'UserProfile.isCompanyAdmin'
+        db.delete_column('onBaristaApp_userprofile', 'isCompanyAdmin')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'onBaristaApp.checkin': {
+            'Meta': {'object_name': 'checkIn'},
+            'barista': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'checkedin': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inTime': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['onBaristaApp.companyLocation']"}),
+            'outTime': ('django.db.models.fields.DateTimeField', [], {'null': 'True'})
+        },
+        'onBaristaApp.company': {
+            'Meta': {'object_name': 'Company'},
+            'companyContact': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'companyName': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'onBaristaApp.companylocation': {
+            'Meta': {'object_name': 'companyLocation'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'companyID': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['onBaristaApp.Company']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'default': "'MA'", 'max_length': '2', 'blank': 'True'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'zipCode': ('django.db.models.fields.CharField', [], {'max_length': '5', 'blank': 'True'})
+        },
+        'onBaristaApp.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'favBaristaObj': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['onBaristaApp.UserProfile']", 'null': 'True', 'blank': 'True'}),
+            'favCompany': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['onBaristaApp.Company']", 'null': 'True', 'blank': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isCompanyAdmin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'mug': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'userType': ('django.db.models.fields.CharField', [], {'default': "'Consumer'", 'max_length': '10'}),
+            'usercheckedin': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['onBaristaApp']
