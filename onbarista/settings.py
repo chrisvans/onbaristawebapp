03a6a3de2e87b0553c1571b501e731565506c69c
chrisvans <chrisvanschyndel@gmail.com>
2013-07-01 23:18:34 -0400

    Memories.

--- onbarista/settings.py
+++ onbarista/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/New York'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -48,7 +48,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
-MEDIA_ROOT = MEDIA_BASE_DIR + '/onBaristaApp/static/Media'
+MEDIA_ROOT = MEDIA_BASE_DIR + '/onBaristaApp/static/Media/'
 #MEDIA_ROOT = '/Users/LBMBPro/desktop/devrampup/onbaristawebapp/onBaristaApp/static/Media'
 #MEDIA_ROOT = '/Users/Chris Van Schyndel/desktop/obaristawebapp/onBaristaApp/static/Media'
 #MEDIA_ROOT = '/Users/Gigglande/desktop/obaristawebapp/onBaristaApp/static/Media'
@@ -56,7 +56,7 @@ MEDIA_ROOT = MEDIA_BASE_DIR + '/onBaristaApp/static/Media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'Media'
+MEDIA_URL = '/Media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
