56393668424de9706851c0dc4228832ed49a02ff
chrisvans <chrisvanschyndel@gmail.com>
2013-07-31 13:27:25 -0400

    Settings muddling 2

--- onbarista/urls.py
+++ onbarista/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #urlpatterns += patterns('',
 #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
